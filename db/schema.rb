9f61ab2a20d06cf5dd1c76504396aee51794ff7d
James <james.jcarline@gmail.com>
2023-04-17 15:21:41 +0100

    Add user_id column to User table db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_071208) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_141734) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_071208) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
 end
