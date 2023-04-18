131f83e9d29111bbdcb5fbf9dfa172080677b390
jamescln <james.jcarline@gmail.com>
2023-04-18 08:16:31 +0100

    Add validations and associations to Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  validates :body, presence: true, length: { in: 1..50 }
+  validates :likes, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :post
+  belongs_to :user
 end
