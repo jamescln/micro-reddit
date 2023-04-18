921b7b22e782ebd36b807a926976e7bb293cdb74
jamescln <james.jcarline@gmail.com>
2023-04-18 07:57:02 +0100

    Add user_id and likes to posts table

---
+++ db/migrate/20230418065504_add_column_to_posts.rb
@@ -0,0 +1,6 @@
+class AddColumnToPosts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :user_id, :integer
+    add_column :posts, :likes, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_141734) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_065504) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "likes"
   end
 
   create_table "users", force: :cascade do |t|
